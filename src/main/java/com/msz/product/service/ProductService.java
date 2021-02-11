19bc0b1fb316d39225a61aad16205f9571d80a37
mariusz_sz_iFLOW <mariusz.szmer@iflow.pl>
2021-02-11 16:52:27 +0100

    fix constructor problem

--- src/main/java/com/msz/product/service/ProductService.java
+++ src/main/java/com/msz/product/service/ProductService.java
@@ -2,6 +2,8 @@ package com.msz.product.service;
 
 import com.msz.product.ProductRepository;
 import com.msz.product.model.Product;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,7 +11,10 @@ import java.util.List;
 @Service
 public class ProductService {
 
-    private final ProductRepository productRepository;
+    private ProductRepository productRepository;
+
+    public ProductService() {
+    }
 
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
