c8c24efd8bb7c91b9164c9d0c2b46d2d78a10285
marszm <mariusz.szmer@gmail.com>
2021-02-13 10:45:48 +0100

    added swagger2 for documentation

--- src/main/java/com/msz/product/controller/ProductController.java
+++ src/main/java/com/msz/product/controller/ProductController.java
@@ -4,8 +4,6 @@ import com.msz.product.model.Product;
 import com.msz.product.service.ProductService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,7 +22,7 @@ public class ProductController {
     @PostMapping("/addProduct")
     ResponseEntity<Product> addProduct(@RequestBody Product product) {
         String status = productService.addProduct(product);
-        log.info("product added status", status);
+        log.info(status);
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
     }
 
