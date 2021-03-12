84b672a45da8998950747b927fb1a85d22d983dc
marszm <mariusz.szmer@gmail.com>
2021-03-12 17:29:58 +0100

    added integration test to productById method v1

--- src/test/java/com/msz/product/controller/ProductControllerTest.java
+++ src/test/java/com/msz/product/controller/ProductControllerTest.java
@@ -1,13 +1,17 @@
 package com.msz.product.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.msz.product.model.Product;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
@@ -18,15 +22,23 @@ class ProductControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
     void shouldGetSingleProduct() throws Exception {
         // given
         // when
-        mockMvc.perform(get("/v1/products/602aaa39f96cda08c46fd331"))
+        MvcResult mvcResult = mockMvc.perform(get("/v1/products/602aaa39f96cda08c46fd331"))
                 .andDo(print())
                 .andExpect(MockMvcResultMatchers.status().is(200))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is("602aaa39f96cda08c46fd331")));
+                .andReturn();
         // then
+        Product product = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), Product.class);
+        assertThat(product).isNotNull();
+        assertThat(product.getId()).isEqualTo("602aaa39f96cda08c46fd331");
+        assertThat(product.getName()).isEqualTo("dsadasd");
+        assertThat(product.getDiscount()).isEqualTo(10.0);
     }
 
 }
